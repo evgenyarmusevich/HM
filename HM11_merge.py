b838e65fcd142703fad59eb20e8884bf369984ce
evgenyarmusevich <e.yarmusevich@gmail.com>
2021-09-02 11:59:35 +0300

    1

--- HM11_merge.py
+++ HM11_merge.py
@@ -1,18 +1,17 @@
 import random
 def rgen():
-    a = 10
     b = 0
     c = 10
     list1 = []
     list2 = []
-    for i in range(a):
+    for i in range(c):
         list1.append(random.randint(b, c))
         list2.append(random.randint(b, c))
     print("Случайный список 1: ", list1)
     print("Случайный список 2: ", list2)
 
     newlist = {x for x in list1 if x in list2}
-    print("Список с общими значениями: ", newlist)
+    print("Список с общими значениями: ", list(newlist))
 
 rgen()
 
