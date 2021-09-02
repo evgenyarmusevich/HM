c44c49172edb7ea930f8b77eeb61093ebeae43b9
evgenyarmusevich <e.yarmusevich@gmail.com>
2021-09-02 11:18:04 +0300

    Delete GoodsVodaVdom.xml

--- HM10_count.py
+++ HM10_count.py
@@ -1,11 +1,13 @@
 def count():
-listn=str(input("Введите число: "))
-print(listn)
-k=0
-for elem in listn:
-    if not elem.isdigit():
-        print("Введенные данные не являются числом")
-        break
-    if elem.isdigit():
-        k += int(elem)
-print("Сумма чисел =", k)
+    listn=str(input("Введите число: "))
+    print(listn)
+    k=0
+    for elem in listn:
+        if not elem.isdigit():
+            print("Введенные данные не являются числом")
+            break
+        if elem.isdigit():
+            k += int(elem)
+    print("Сумма чисел =", k)
+
+count()
