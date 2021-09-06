d5d54a42464b4a374fe132494bca57ba4700813f
evgenyarmusevich <e.yarmusevich@gmail.com>
2021-09-06 14:19:22 +0300

    правки

--- HM9_Fchange.py
+++ HM9_Fchange.py
@@ -1,22 +1,28 @@
 import random
+a = int(input("Введите длину списка: "))
+b = int(input("Введите минимальное значение списка: "))
+c = int(input("Введите максимальное значение списка: "))
+list1 = []
 
 def rgen():
-    a = 10
-    b = int(input("First nuber:"))
-    c = int(input("Second number:"))
-    list = []
     for i in range(a):
         if b < c:
-            list.append(random.randint(b, c))
+            list1.append(random.randint(b, c))
         else:
-            list.append(random.randint(c, b))
+            list1.append(random.randint(c, b))
+    print(list1)
 
-    print(list)
+def nfunc():
+    list2 = list1.copy()
+    cnum = int(input("Введите число от 0 до " + str(a - 1) + " "))
+    if cnum >= a:
+        print("Введенное число выходит за пределы списка!")
+        nfunc()
+    else:
+        list1.pop(cnum)
+        list1.insert(cnum, cnum)
+        print("Модифицированный список " + str(list1))
+        print("Число " + str(list2[cnum]) + " замененно на " + str(cnum))
 
-    cnum = int(input("Third nuber:"))
-    maxl = list.index(max(list))
-    list[maxl] = cnum
-    print(list)
 
-
-rgen()
+rgen(), nfunc()
