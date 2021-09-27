f8742824fa7a86758d572745e44080ccc84b7fa6
evgenyarmusevich <e.yarmusevich@gmail.com>
2021-09-27 10:06:28 +0300

    HM 14

--- HM14_Menu.py
+++ HM14_Menu.py
@@ -3,11 +3,12 @@ from tkinter import *
 main_menu = Menu()
 
 file_menu = Menu()
-
-file_menu.add_command(label="Создать запись")
-file_menu.add_command(label="Найти запись")
-file_menu.add_command(label="Редактировать запись")
-file_menu.add_command(label="Удалить запись")
-file_menu.add_command(label="Выйти из программы")
+def zagl():
+    print("В разработке")
+file_menu.add_command(label="Создать запись", command=zagl)
+file_menu.add_command(label="Найти запись", command=zagl)
+file_menu.add_command(label="Редактировать запись", command=zagl)
+file_menu.add_command(label="Удалить запись", command=zagl)
+file_menu.add_command(label="Выйти из программы", command=zagl)
 
 main_menu.add_cascade(label="Главное меню", menu=file_menu)
